19d83f82f4943d53312eb03f950dc925e80999d2
Jiří Novák <mail@novakjiri.cz>
2025-01-25 12:02:41 +0100

    Revert "Anaku"

--- npc_3.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D  # Or the NPC's root node type
-
-@onready var animated_sprite = $AnimatedSprite2D  # Path to the AnimatedSprite2D node
-
-func _ready():
-	# Set the animation to play automatically
-	animated_sprite.play("default")
